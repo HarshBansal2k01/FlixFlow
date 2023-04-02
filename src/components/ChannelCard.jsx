b1aa34630534ae9daba0a64dde98a60759e49f5c
HarshBansal2k01 <ihbbro14@gmail.com>
2023-04-02 20:31:48 +0530

    done

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constant";
